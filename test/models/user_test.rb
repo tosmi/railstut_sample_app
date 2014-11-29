289f4b4f33fbcba0a1412d5145113babc8721e4d
Toni Schmidbauer <toni@stderr.at>
2014-11-29 21:21:40 +0100

    chap 6 exercises finished

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -39,11 +39,18 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email validation should reject invalid addresses" do
-    invalid_addresses = %w[ user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com ]
+    invalid_addresses = %w[ user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com foo@bar..com ]
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
+  end
+
+  test "email address should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAmPlE.cOm"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
   end
 
   test "email addresses should be unique" do
