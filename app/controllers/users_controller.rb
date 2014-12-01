e9ff90b04a7e3914035084f4d9c4ce6fd41ecb61
Toni Schmidbauer <toni@stderr.at>
2014-12-01 22:05:51 +0100

    finished 7.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,25 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[user_params])
+    if @user.save
+      #successful
+    else
+      render 'new'
+    end
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email,
+                                 :password, :password_confirmation)
   end
 end
