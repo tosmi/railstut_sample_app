d448b4dd4fb04a6e1cc4d365e138ede24592d818
Toni Schmidbauer <toni@stderr.at>
2014-12-02 22:36:29 +0100

    chapter 7 exercises finished

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,6 +10,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert'
+    assert_select 'div.alert-danger'
   end
 
   test "valid signup information" do
@@ -22,5 +25,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                              }
     end
     assert_template 'users/show'
+    assert_not flash.empty?, 'flash should not be empty on successful signup'
   end
 end
