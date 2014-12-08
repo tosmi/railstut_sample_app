24ecd960f49e67c4f02d160cbcb1679ad572ee83
Toni Schmidbauer <toni@stderr.at>
2014-12-08 21:32:13 +0100

    9.2 done

--- test/integration/user_edit_test.rb
+++ test/integration/user_edit_test.rb
@@ -7,6 +7,7 @@ class UserEditTest < ActionDispatch::IntegrationTest
   end
 
   test "unsuccessful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), user: { name: "",
@@ -15,5 +16,23 @@ class UserEditTest < ActionDispatch::IntegrationTest
                                     password_confirmation: "bar",
                                   }
     assert_template 'users/edit'
+  end
+
+  test "successful edit" do
+    get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_path(@user)
+    name = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), user: { name: name,
+                                    email: email,
+                                    password: "",
+                                    password_confirmation: ""
+                                  }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal @user.name, name
+    assert_equal @user.email, email
   end
 end
