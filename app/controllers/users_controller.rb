2f4506616098a7e2e5fd11313c8fcc4628d7e8d4
Toni Schmidbauer <toni@stderr.at>
2014-12-09 22:12:28 +0100

    9.3.3 done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
 
   def new
@@ -15,6 +15,10 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
   end
 
   def show
