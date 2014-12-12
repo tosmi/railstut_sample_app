ac86b6a2bf27ee88ffa0152b302d9e2482a7e41c
Toni Schmidbauer <toni@stderr.at>
2014-12-12 19:57:51 +0100

    9.6 exercise 1/2 done

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,7 +2,11 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
-  test "layout links" do
+  def setup
+    @user = users(:michael)
+  end
+
+  test "layout links without logging in" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
@@ -10,6 +14,20 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", login_path
+  end
+
+  test "layout links with logging in" do
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", root_path
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
   end
 
   test "signup links" do
--- test/integration/user_edit_test.rb
+++ test/integration/user_edit_test.rb
@@ -22,6 +22,7 @@ class UserEditTest < ActionDispatch::IntegrationTest
     get edit_user_path(@user)
     log_in_as(@user)
     assert_redirected_to edit_user_path(@user)
+    assert_nil session[:forwarding_url]
     name = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), user: { name: name,
