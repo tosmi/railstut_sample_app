8beb72bb36d742250d2f2b6799ea5bcffacf42ff
Toni Schmidbauer <toni@stderr.at>
2014-12-20 21:15:57 +0100

    11.1.2 done

---
+++ test/models/microposts_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class MicropostsTest < ActiveSupport::TestCase
+
+  def setup
+    @user = users(:michael)
+    @micropost = Microposts.new(content: "Lorem ipsum", user_id: @user.id)
+  end
+
+  test "micropost should be valid" do
+    assert @micropost.valid?
+  end
+
+  test "user id should be present" do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+
+end
