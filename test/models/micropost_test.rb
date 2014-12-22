387cb65c2b89636e98b1bb4190638eadb4561aba
Toni Schmidbauer <toni@stderr.at>
2014-12-22 20:43:10 +0100

    11.1 done

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -16,6 +16,16 @@ class MicropostTest < ActiveSupport::TestCase
     assert_not @micropost.valid?
   end
 
+  test "content should be present" do
+    @micropost.content = " "
+    assert_not @micropost.valid?
+  end
+
+  test "content should be at most 140 characters" do
+    @micropost.content = "a" * 141
+    assert_not @micropost.valid?
+  end
+
   test "order should be most recent first" do
     assert_equal Micropost.first, microposts(:most_recent)
   end
