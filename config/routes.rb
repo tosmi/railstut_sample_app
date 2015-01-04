31118d292e584d8bc08b6f50a3ca539222ff15a6
Toni Schmidbauer <toni@stderr.at>
2015-01-04 22:27:24 +0100

    routes for relationships done

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
-  resources :user do
+  resources :users do
     member do
       get :following, :followers
     end
@@ -22,4 +22,5 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
