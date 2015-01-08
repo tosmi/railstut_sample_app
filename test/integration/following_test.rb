e354b2ecb2707b65fa4085815584920b89a7517d
Toni Schmidbauer <toni@stderr.at>
2015-01-08 17:19:26 +0100

    12.2 done

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -4,6 +4,7 @@ class FollowingTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:michael)
+    @other_user = users(:archer)
     log_in_as(@user)
   end
 
@@ -23,6 +24,29 @@ class FollowingTest < ActionDispatch::IntegrationTest
     @user.followers.each do |user|
 
       assert_select "a[href=?]", user_path(user)
+    end
+  end
+
+  test "should follow a user do" do
+    assert_difference '@user.following.count', 1 do
+      post relationships_path, followed_id: @other_user.id
+    end
+    @user.unfollow(@other_user)
+    assert_difference '@user.following.count', 1 do
+      xhr :post, relationships_path, followed_id: @other_user.id
+    end
+  end
+
+  test "should unfollow a user do" do
+    @user.follow(@other_user)
+    relationship = @user.active_relationships.find_by(followed_id: @other_user.id)
+    assert_difference '@user.following.count', -1 do
+      delete relationship_path(relationship), relationship_id: relationship.id
+    end
+    @user.follow(@other_user)
+    relationship = @user.active_relationships.find_by(followed_id: @other_user.id)
+    assert_difference '@user.following.count', -1 do
+      xhr :delete, relationship_path(relationship), relationship_id: relationship.id
     end
   end
 end
